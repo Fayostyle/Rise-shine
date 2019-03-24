4f74790f8734f56837ff43a52823eec7cb2b5927
fayostyle <phuang5945@163.com>
2019-03-24 23:55:28 +0800

    jumpGame dynamic solution

--- src/main/java/leetcode/LC55_JumpGame.java
+++ src/main/java/leetcode/LC55_JumpGame.java
@@ -21,6 +21,39 @@ public class LC55_JumpGame {
         return false;
     }
 
+
+    enum Index {
+        GOOD, BAD, UNKNOWN
+    }
+
+    Index[] memo;
+    public boolean canJumpFromPosition2(int position, int[] nums) {
+        if(memo[position] != Index.UNKNOWN) {
+            return memo[position] == Index.GOOD ? true : false;
+        }
+
+        int furthestJump = Math.min(nums.length - 1, nums[position] + position);
+        for(int i=position + 1; i<=furthestJump; i++) {
+            if(canJumpFromPosition2(i, nums)) {
+                memo[position] = Index.GOOD;
+                return true;
+            }
+        }
+
+        memo[position] = Index.BAD;
+        return false;
+    }
+
+    public boolean canJump2(int[] nums) {
+        memo = new Index[nums.length];
+        for(int i=0 ;i<nums.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+
+        memo[nums.length-1] = Index.GOOD;
+        return canJumpFromPosition2(0, nums);
+    }
+
     public static void main(String[] args) {
         canJump(new int[]{3,2,1,0,4});
     }
